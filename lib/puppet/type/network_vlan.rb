ca6a489d13509afe499871b670680605c2edf87b
Jeff McCune <jeff@puppetlabs.com>
2014-07-02 10:12:33 -0700

    (ND-34) Rename netdev_vlan to network_vlan
    As per V1 specification document at
    http://links.puppetlabs.com/netdev_v1_spec

--- lib/puppet/type/network_vlan.rb
+++ lib/puppet/type/network_vlan.rb
@@ -1,66 +1,27 @@
-=begin
-* Puppet Module  : netdev
-* Author         : Jeremy Schulman
-* File           : puppet/type/netdev_vlan.rb
-* Version        : 2012-11-11
-* Description    :
-*
-*    This file contains the Type definition for the network
-*    device Vlan resource.
-*
-=end
-
-Puppet::Type.newtype(:netdev_vlan) do
-  @doc = "Network Device VLAN"
+Puppet::Type.newtype(:network_vlan) do
+  @doc = "Manage VLAN's.  Layer-2 VLAN's are managed by this resource type."
 
   ensurable
-  feature :activable, "The ability to activate/deactive configuration"
-  feature :describable, "The ability to add a description"
-  feature :no_mac_learning, "The ability disable MAC learning"
 
-  ##### -------------------------------------------------------------
-  ##### Parameters
-  ##### -------------------------------------------------------------
+  feature :describable, "VLAN descriptions are supported by the platform"
+
+  # Parameters (additional data)
 
   newparam(:name, :namevar=>true) do
-    desc "The VLAN name"
+    desc <<-EOT
+      The resource identifier.  The value does not affect the configuration and
+      instead is used to identify this resource within Puppet and related
+      tools.
+    EOT
   end
 
-  ##### -------------------------------------------------------------
-  ##### Properties
-  ##### -------------------------------------------------------------
-
-  newproperty(:active, :required_features => :activable) do
-    desc "Config activation"
-    defaultto(:true)
-    newvalues(:true, :false)
-  end
+  # Properties (state management)
 
   newproperty(:description, :required_features => :describable) do
     desc "The VLAN Description"
   end
 
-  newproperty(:vlan_id) do
-    desc "The VLAN ID"
+  newproperty(:id) do
+    desc "The VLAN ID, e.g. 100"
   end
-
-  newproperty(:no_mac_learning, :required_features => :no_mac_learning) do
-    desc "Do not learn MAC addresses; used for 2-port VLANs"
-    defaultto(:false)
-    newvalues(:true, :false)
-  end
-
-  ##### -------------------------------------------------------------
-  ##### Auto require the netdev_device resource -
-  #####   There must be one netdev_device resource defined in the
-  #####   catalog, it doesn't matter what the name of the device is,
-  #####   just that one exists.
-  ##### -------------------------------------------------------------
-
-  autorequire(:netdev_device) do
-    netdev = catalog.resources.select{ |r| r.type == :netdev_device }[0]
-    raise "No netdev_device found in catalog" unless netdev
-    netdev.title   # returns the name of the netdev_device resource
-  end
-
 end
