2947cb2710362263fa8e447ced512ef23259148b
Jeff McCune <jeff@puppetlabs.com>
2014-08-28 14:36:54 -0700

    (NETDEV-5) Add name_server type

---
+++ lib/puppet/type/name_server.rb
@@ -0,0 +1,17 @@
+# encoding: utf-8
+
+Puppet::Type.newtype(:name_server) do
+  @doc = 'Configure the resolver to use the specified DNS server'
+
+  ensurable
+
+  newparam(:name, namevar: true) do
+    desc 'The hostname or address of the DNS server'
+
+    validate do |value|
+      if value.is_a? String then super(value)
+      else fail "value #{value.inspect} is invalid, must be a String."
+      end
+    end
+  end
+end
