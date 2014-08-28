7fa57bdbe523fe756277b2bf2688e3450a3c6f22
Jeff McCune <jeff@puppetlabs.com>
2014-08-28 16:23:21 -0700

    (NETDEV-12) Add radius_server_group type

---
+++ lib/puppet/type/radius_server_group.rb
@@ -0,0 +1,25 @@
+# encoding: utf-8
+
+Puppet::Type.newtype(:radius_server_group) do
+  @doc = 'Configure a radius server group'
+
+  newparam(:name, namevar: true) do
+    desc 'The name of the radius server group'
+
+    validate do |value|
+      if value.is_a? String then super(value)
+      else fail "value #{value.inspect} is invalid, must be a String."
+      end
+    end
+  end
+
+  newproperty(:servers, array_matching: :all) do
+    desc 'Array of servers associated with this group'
+
+    validate do |value|
+      if value.is_a? String then super(value)
+      else fail "value #{value.inspect} is invalid, must be a String."
+      end
+    end
+  end
+end
