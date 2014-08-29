2d04bdfcf5d48c7260890326e527c86e5d72e044
Jeff McCune <jeff@puppetlabs.com>
2014-08-29 15:33:14 -0700

    (NETDEV-13) Add tacacs_server_group type

---
+++ spec/unit/puppet/type/tacacs_server_group_spec.rb
@@ -0,0 +1,12 @@
+# encoding: utf-8
+
+require 'spec_helper'
+
+describe Puppet::Type.type(:tacacs_server_group) do
+  let(:catalog) { Puppet::Resource::Catalog.new }
+  let(:type) { described_class.new(name: 'emanon', catalog: catalog) }
+  subject { described_class.attrclass(attribute) }
+
+  it_behaves_like 'name is the namevar'
+  it_behaves_like 'array of strings property', attribute: :servers
+end
