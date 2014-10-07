692b8c307ca15954bb7363b7a1b16646bd75c63e
Jeff McCune <jeff@puppetlabs.com>
2014-10-07 10:52:54 -0700

    (NETDEV-17) Add network_snmp type
    This patch adds the network_snmp type as recommended by our partners.
    This type consolidates the information from the previous location,
    contact, and overall enable types.

---
+++ spec/unit/puppet/type/network_snmp_spec.rb
@@ -0,0 +1,22 @@
+# encoding: utf-8
+
+require 'spec_helper'
+
+describe Puppet::Type.type(:network_snmp) do
+  let(:catalog) { Puppet::Resource::Catalog.new }
+  let(:type) { described_class.new(name: 'emanon', catalog: catalog) }
+
+  subject { described_class.attrclass(attribute) }
+
+  it_behaves_like 'name is the namevar'
+  it_behaves_like 'an ensurable type'
+  it_behaves_like 'enabled type'
+
+  [:contact, :location].each do |param|
+    describe param.to_s do
+      let(:attribute) { param }
+      include_examples '#doc Documentation'
+      include_examples 'string value'
+    end
+  end
+end
